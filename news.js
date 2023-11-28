d603d7722392776056a736eba6e2f9f2c56bcd3f
jsango2 <jure.sango@gmail.com>
2023-11-28 07:08:45 +0100

    first commit

--- news.js
+++ news.js
@@ -9,32 +9,25 @@ export const news = [
   },
   {
     id: 2,
-
     datum: "3.9.2022",
     naslov: "NOVOST BROJ 2",
     naslovEng: "News No2",
-
     text: "U kategorijama svježih, smrznutih i rashlađenih proizvoda...",
     link: "https://www.cromaris.hr",
   },
   {
     id: 3,
-
     datum: "6.9.2022",
     naslov: "NOVOST BROJ 3",
     naslovEng: "News No3",
-
     text: "U kategorijama svježih, smrznutih i rashlađenih proizvoda...",
     link: "https://www.cromaris.hr",
   },
-  ,
   {
     id: 4,
-
     datum: "9.9.2024",
     naslov: "NOVOST BROJ 4",
     naslovEng: "News No4",
-
     text: "U kategorijama svježih, smrznutih i rashlađenih proizvoda...",
     link: "https://www.cromaris.hr",
   },
