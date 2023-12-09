3d829be08a56b9672be8e6064fd28797dab096bd
jsango2 <jure.sango@gmail.com>
2023-12-09 13:58:11 +0100

    daljnji dizajn. proizvodi. novosti...

--- pages/_document.js
+++ pages/_document.js
@@ -44,6 +44,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bangers&family=Montserrat:wght@400;500;600;700;900&display=swap"
             rel="stylesheet"
           ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         {/* <title>Your site title</title> */}
         {/* {this.props.styleTags} */}
