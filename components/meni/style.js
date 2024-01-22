004855f62e0deb64b411a1884c857b4f0922d664
jsango2 <jure.sango@gmail.com>
2024-01-22 18:00:53 +0100

    meni redesign mobile

--- components/meni/style.js
+++ components/meni/style.js
@@ -95,6 +95,8 @@ export const BottomBlock = styled.div`
   display: flex;
   flex-direction: column;
   background: url("/kontaktBgImg.webp");
+  background-size: cover;
+  background-position: center;
   width: 100%;
   height: 100%;
   align-items: center;
