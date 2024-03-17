c2e4f9200866646eecf0bada1438d3d5281e84bd
jsango2 <jure.sango@gmail.com>
2024-03-17 19:07:13 +0100

    seo i og images

--- pages/proizvodi/index.js
+++ pages/proizvodi/index.js
@@ -6,10 +6,74 @@ import { catalogData } from "../../catalogData";
 import ProizvodiPage from "../../components/ProizvodiPage";
 import { getAllPhotosProizvodi } from "../../lib/api2.js";
 import Hero from "../../components/ProizvodiPage/Hero/hero.js";
-
+import { useRouter } from "next/router.js";
+import Head from "next/head";
 function index() {
+  const router = useRouter();
+  const { locale } = router;
   return (
     <Layout>
+      <Head>
+        <title>Marikomerc - Proizvodi</title>
+        <link
+          rel="canonical"
+          href="https://www.marikomerc.hr/proizvodi"
+          key="canonical"
+        />
+
+        <meta
+          property="og:title"
+          content="Marikomerc - Proizvodi"
+          key="title"
+        />
+        <meta name="twitter:title" content="Marikomerc - Proizvodi" />
+        <meta
+          name="twitter:description"
+          content={
+            locale === "hr"
+              ? "Nudimo širok izbor morskih proizvoda - smrznute, svježe, dimljene, sušene, marinirane i usoljene varijante, uključujući i McCain program, smrznuto povrće, tjesteninu, meso te veganske zamjene, zadovoljavajući raznolike prehrambene i kulinarske potrebe."
+              : "We offer a wide range of seafood products including frozen, fresh, smoked, dried, marinated, and salted varieties, along with the McCain program, frozen vegetables, pasta, meat, and vegan substitutes, catering to diverse dietary and culinary needs."
+          }
+        />
+        <meta
+          name="twitter:image"
+          content={
+            locale === "hr" ? "/proizvodiOGhr.png" : "/proizvodiOGeng.png"
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="description"
+          content={
+            locale === "hr"
+              ? "Nudimo širok izbor morskih proizvoda - smrznute, svježe, dimljene, sušene, marinirane i usoljene varijante, uključujući i McCain program, smrznuto povrće, tjesteninu, meso te veganske zamjene, zadovoljavajući raznolike prehrambene i kulinarske potrebe."
+              : "We offer a wide range of seafood products including frozen, fresh, smoked, dried, marinated, and salted varieties, along with the McCain program, frozen vegetables, pasta, meat, and vegan substitutes, catering to diverse dietary and culinary needs."
+          }
+          key="desc"
+        />
+        <meta
+          property="og:description"
+          content={
+            locale === "hr"
+              ? "Nudimo širok izbor morskih proizvoda - smrznute, svježe, dimljene, sušene, marinirane i usoljene varijante, uključujući i McCain program, smrznuto povrće, tjesteninu, meso te veganske zamjene, zadovoljavajući raznolike prehrambene i kulinarske potrebe."
+              : "We offer a wide range of seafood products including frozen, fresh, smoked, dried, marinated, and salted varieties, along with the McCain program, frozen vegetables, pasta, meat, and vegan substitutes, catering to diverse dietary and culinary needs."
+          }
+        />
+        <meta
+          property="og:url"
+          content={
+            locale === "hr"
+              ? `"https://www.marikomerc.hr/proizvodi"`
+              : `"https://www.marikomerc.hr/en/proizvodi"`
+          }
+        />
+        <meta
+          property="og:image"
+          content={
+            locale === "hr" ? "/proizvodiOGhr.png" : "/proizvodiOGeng.png"
+          }
+        />
+      </Head>
       <Hero />
       <ProizvodiPage />
     </Layout>
