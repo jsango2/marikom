a6e46271226c97582a8a544164466454ebeb8362
jsango2 <jure.sango@gmail.com>
2024-03-17 19:04:03 +0100

    seo i og images

--- pages/karijere/index.js
+++ pages/karijere/index.js
@@ -8,13 +8,69 @@ import Layout from "../../components/layout";
 import { getAllOglasi } from "../../lib/api2";
 import slugify from "slugify";
 import Link from "next/link";
-
+import { useRouter } from "next/router.js";
+import Head from "next/head";
 function Karijere(props) {
+  const router = useRouter();
+  const { locale } = router;
   const oglasi = props.oglasi;
 
   // console.log(props);
   return (
     <Layout>
+      <Head>
+        <title>Marikomerc - Karijere</title>
+        <link
+          rel="canonical"
+          href="https://www.marikomerc.hr/karijere"
+          key="canonical"
+        />
+
+        <meta property="og:title" content="Marikomerc - Karijere" key="title" />
+        <meta name="twitter:title" content="Marikomerc - Karijere" />
+        <meta
+          name="twitter:description"
+          content={
+            locale === "hr"
+              ? "Marikomerc nudi karijerne prilike za ambiciozne i timski orijentirane pojedince. Fokusirani smo na razvoj vještina i profesionalni rast te pozivamo kandidate s relevantnim kompetencijama da se pridruže našem timu."
+              : "Marikomerc offers career opportunities for ambitious and team-oriented individuals. We are focused on skill development and professional growth, inviting candidates with relevant competencies to join our team."
+          }
+        />
+        <meta
+          name="twitter:image"
+          content={locale === "hr" ? "/karijereOGhr.png" : "/karijereOGeng.png"}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="description"
+          content={
+            locale === "hr"
+              ? "Marikomerc nudi karijerne prilike za ambiciozne i timski orijentirane pojedince. Fokusirani smo na razvoj vještina i profesionalni rast te pozivamo kandidate s relevantnim kompetencijama da se pridruže našem timu."
+              : "Marikomerc offers career opportunities for ambitious and team-oriented individuals. We are focused on skill development and professional growth, inviting candidates with relevant competencies to join our team."
+          }
+          key="desc"
+        />
+        <meta
+          property="og:description"
+          content={
+            locale === "hr"
+              ? "Marikomerc nudi karijerne prilike za ambiciozne i timski orijentirane pojedince. Fokusirani smo na razvoj vještina i profesionalni rast te pozivamo kandidate s relevantnim kompetencijama da se pridruže našem timu."
+              : "Marikomerc offers career opportunities for ambitious and team-oriented individuals. We are focused on skill development and professional growth, inviting candidates with relevant competencies to join our team."
+          }
+        />
+        <meta
+          property="og:url"
+          content={
+            locale === "hr"
+              ? `"https://www.marikomerc.hr/karijere"`
+              : `"https://www.marikomerc.hr/en/karijere"`
+          }
+        />
+        <meta
+          property="og:image"
+          content={locale === "hr" ? "/karijereOGhr.png" : "/karijereOGeng.png"}
+        />
+      </Head>
       <MasterContainer>
         <AppHeader />
         <CompanyLogos />
