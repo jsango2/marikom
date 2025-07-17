76023836a21b7bf6ae2469228e296a5d34c5b8ff
jsango2 <jure.sango@gmail.com>
2025-07-17 16:32:06 +0200

    revalidate novosti 90 sec

--- pages/novosti/[slug].js
+++ pages/novosti/[slug].js
@@ -207,7 +207,7 @@ export async function getStaticPaths({ locales }) {
     // });
   });
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
